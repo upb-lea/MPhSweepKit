57581e361c5b9beb1b09d33c96999d5c6c3353c1
Till Piepenbrock <piepenbrock@lea.upb.de>
2026-07-02 15:54:33 +0200

    add creation of batch data directory.

--- mphsweepkit/utils/__init__.py
+++ mphsweepkit/utils/__init__.py
@@ -2,5 +2,5 @@
 from .io import read_comsol_txt_to_df
 from .parameters import get_parameters, print_parameters, get_descriptions
 from .plotting import plot_comsol_df
-from .sweeps import get_sweep_data, set_parametric_sweep, get_frequency_data, get_cascaded_dataset
+from .sweeps import get_sweep_data, set_parametric_sweep, get_frequency_data, get_cascaded_dataset, set_batch_directory, set_server_directory
 from .postprocessing import load_post_processing_exprs
--- mphsweepkit/utils/sweeps.py
+++ mphsweepkit/utils/sweeps.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import numpy as np
 from numpy.typing import NDArray
 import pandas as pd
@@ -101,3 +102,42 @@ def get_cascaded_dataset(
     ]
 
     return pd.DataFrame(records)
+
+
+def set_directory(directory_name: str):
+    """
+    Set directory for the geometric sweep to the absolute path of the specified directory.
+
+    :param directory_name: Name of the directory to set.
+    """
+    if not Path(directory_name).exists():
+        Path(directory_name).mkdir()
+        print(f"Directory '{directory_name}' created.")
+    else:
+        print(f"Directory '{directory_name}' already exists. Using existing directory.")
+
+
+def set_batch_directory(parametric_sweep: mph.Node, directory_name: str = "batch_data"):
+    """
+    Set batch directory for the geometric sweep to the absolute path of the "server_dir" directory
+
+    :param parametric_sweep: The parametric sweep object to set the batch directory for.
+    :param directory_name: Name of the batch directory to set.
+    """
+    set_directory(directory_name)
+    absolute_path = Path(directory_name).absolute()
+    parametric_sweep.property("batchdir", str(absolute_path))
+    print(f"Batch directory for the geometric sweep set to: {absolute_path}")
+
+
+def set_server_directory(parametric_sweep: mph.Node, directory_name: str = "server_dir"):
+    """
+    Set server directory for the geometric sweep to the absolute path of the "server_dir" directory
+
+    :param parametric_sweep: The parametric sweep object to set the server directory for.
+    :param directory_name: Name of the server directory to set.
+    """
+    set_directory(directory_name)
+    absolute_server_path = Path(directory_name).absolute()
+    parametric_sweep.property("serverdir", str(absolute_server_path))
+    print(f"Server directory for the geometric sweep set to: {absolute_server_path}")
