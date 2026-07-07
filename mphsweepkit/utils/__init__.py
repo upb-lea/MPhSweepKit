ca5328dc0971133d51b4de5202a3548d2e7bf050
Till Piepenbrock <piepenbrock@lea.upb.de>
2026-07-07 11:15:18 +0200

    moved utility functions to the main package and removed the utils subpackage

--- mphsweepkit/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""MPhSweepKit utilities for working with COMSOL sweeps."""
-from .io import *
-from .parameters import *
-from .plotting import *
-from .sweeps import *
-from .postprocessing import *
-from .directories import *
