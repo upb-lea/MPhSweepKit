e0ee88dc566e92a483c715e7b89a7493df147471
Till Piepenbrock <piepenbrock@lea.upb.de>
2026-07-07 11:28:34 +0200

    updated the example to use the new mphsweepkit package instead of the utils module.

--- mphsweepkit/__init__.py
+++ mphsweepkit/__init__.py
@@ -1,1 +1,8 @@
-"""Initalize the mphsweepkit package."""
+"""Initialize the mphsweepkit package."""
+
+from .sweeps import *
+from .postprocessing import *
+from .plotting import *
+from .parameters import *
+from .io import *
+from .directories import *
