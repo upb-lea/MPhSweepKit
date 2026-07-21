670500cf21a57194c6b95b6960462f0dd512747c
Till Piepenbrock <piepenbrock@lea.upb.de>
2026-07-21 10:22:56 +0200

    modified derivation functions; enabled arbitrary and multiple column derivations

--- mphsweepkit/derive.py
+++ mphsweepkit/derive.py
@@ -39,23 +39,48 @@ def _add_derived(df, source_col, new_col, unit, transform, group="Derived"):
     return df
 
 
-def add_abs(df, source_col, new_col, unit, group="Derived"):
+# General single-column derived data functions
+def from_single_column(df, source_col, new_col, unit, transform, group="Derived"):
+    """
+    transform gets a complex Series and returns a numeric Series.
+    Example transform: lambda z: np.real(z) + np.imag(z)
+    """
+    return _add_derived(df, source_col, new_col, unit, transform, group=group)
+
+
+# General multi-column derived data functions
+def from_multiple_columns(df, source_cols, new_col, unit, func, group="Derived"):
+    """
+    func gets a list of complex Series in the same order as source_cols.
+    Example func: lambda cols: np.real(cols[0]) + np.real(cols[1])
+    """
+    cols = [_as_complex_series(df, c) for c in source_cols]
+    values = func(cols)
+
+    _init_derived_column(df, new_col)
+    _write_data_values(df, new_col, values)
+    _set_meta(df, new_col, unit, group=group)
+    return df
+
+
+# Exemplary single-column derived data functions
+def abs(df, source_col, new_col, unit, group="Derived"):
     return _add_derived(df, source_col, new_col, unit, np.abs, group=group)
 
 
-def add_phase_rad(df, source_col, new_col, unit="rad", group="Derived"):
+def phase_rad(df, source_col, new_col, unit="rad", group="Derived"):
     return _add_derived(df, source_col, new_col, unit, np.angle, group=group)
 
 
-def add_phase_deg(df, source_col, new_col, unit="deg", group="Derived"):
+def phase_deg(df, source_col, new_col, unit="deg", group="Derived"):
     return _add_derived(
         df, source_col, new_col, unit, lambda z: np.degrees(np.angle(z)), group=group
     )
 
 
-def add_real(df, source_col, new_col, unit, group="Derived"):
+def real(df, source_col, new_col, unit, group="Derived"):
     return _add_derived(df, source_col, new_col, unit, np.real, group=group)
 
 
-def add_imag(df, source_col, new_col, unit, group="Derived"):
+def imag(df, source_col, new_col, unit, group="Derived"):
     return _add_derived(df, source_col, new_col, unit, np.imag, group=group)
